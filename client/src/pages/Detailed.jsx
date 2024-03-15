084bc7b2df8cf8b35c079034f40ee1355b2f3ba8
Dachepalli Vasanth kumar <125804168+vasanthdachepalli@users.noreply.github.com>
2024-03-15 13:46:15 +0530

    alternative node for server is provided

--- client/src/pages/Detailed.jsx
+++ client/src/pages/Detailed.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import '../styles/singlepeod.css';
+
 const SingleProduct = () => {
-  // Access the location object
   const { id } = useParams();
-  const [products, setProducts] = useState([]);
- 
+  const [product, setProduct] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/api/singleproduct/'+id);
-        setProducts(response.data);
-        console.log(response.data);
+        const response = await axios.get(`http://localhost:8080/api/singleproduct/${id}`);
+        setProduct(response.data);
       } catch (error) {
         console.error('Error fetching product data:', error);
       }
@@ -20,12 +19,40 @@ const SingleProduct = () => {
 
     fetchData();
   }, []);
+  const handleAddToCart = () => {
+    // Add logic to send request to backend for adding to cart
+  };
 
-  // Render the component
+  const handleAddToWishlist = () => {
+    
+  };
   return (
     <div>
-      <h2>Single Product</h2>
-     
+      {product && (
+        <div className="single-product-container">
+          <div className="product-photo">
+            <img src={product.photo} alt={product.productName} style={{ width: '50%', height: '50%' }} />
+          </div>
+          <div className="product-details">
+            <h2>{product.productName}</h2>
+            <div className="brand-category">
+              <span className="brand-tag">Brand:{product.brandName}</span>
+              <span className="category-tag">category:{product.category}</span>
+            
+            <p>cost: {product.cost}</p>
+            <p>maxshippingtime: {product.shippingDays}</p>
+            <p>Sellerid: {product.Sellerid}</p>
+            </div>
+            <div className="product-description">
+              <p>{product.productDescription}</p>
+            </div>
+            <div className="action-buttons">
+              <button onClick={handleAddToWishlist}>Add to Wishlist</button>
+              <button onClick={handleAddToCart}>Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
